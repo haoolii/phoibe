94d4e2dbd031f040248f17eac32ea38f204a47dc
Hao <unnhao@gmail.com>
2023-11-30 23:37:19 +0800

    chore: test cors

--- middleware.ts
+++ middleware.ts
@@ -3,10 +3,6 @@ import type { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-    console.log('request', request)
-    request.headers.set('Access-Control-Allow-Origin', 'https://www.safeus.cc/');
-    request.headers.set('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT');
-    request.headers.set('Access-Control-Allow-Headers', 'Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date');
   return NextResponse.next();
 }
  
